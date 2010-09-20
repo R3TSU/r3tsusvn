01a3690990d25c11014e1e70b46ffde3b983f74d
erik.sugiarto@gmail.com <erik.sugiarto@gmail.com@052007a7-1b23-c614-a664-fac8de5c04ab>
2010-09-20 16:16:18 +0000

    remove gameagent, replace with delegating game
    adding initGame
    adding getsuccessorstates

--- 323/Project B/src/ErikssGame.java
+++ 323/Project B/src/ErikssGame.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 
+import p323.partA.Board;
+
 import aima.core.search.adversarial.Game;
 import aima.core.search.adversarial.GameState;
+import aima.core.util.datastructure.XYLocation;
 
 /**
  * 
@@ -11,13 +14,29 @@ import aima.core.search.adversarial.GameState;
  * @author acer
  *
  */
-public class ErikssGame extends Game{
+public class ErikssGame extends Game {
 
 	/**
+	 * @param n
+	 * @param name 
 	 * 
 	 */
-	public ErikssGame() {
+	public ErikssGame(int n, String name) {
 		// TODO Auto-generated constructor stub
+		ArrayList<XYLocation> moves = new ArrayList<XYLocation>();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				XYLocation loc = new XYLocation(i, j);
+				moves.add(loc);
+			}
+		}
+		initialState.put("moves", moves);
+		initialState.put("player", name);
+		initialState.put("utility", new Integer(0));
+		initialState.put("board", new Board(n));
+		initialState.put("level", new Integer(0));
+		presentState = initialState;
+		
 	}
 
 	@Override
@@ -33,19 +52,42 @@ public class ErikssGame extends Game{
 	}
 
 	@Override
-	public int getMiniMaxValue(GameState arg0) {
+	public int getMiniMaxValue(GameState state) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * getSuccessorStates
+	 * implementing abstract method
+	 * generate an arraylist of all possible states after making a move
+	 * based on all different moves available in the current state
+	 * (this does include equivalent moves)
+	 * @param state
+	 */
 	@Override
-	public ArrayList getSuccessorStates(GameState arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<GameState> getSuccessorStates(GameState state) {
+		GameState temp = presentState; // temporary current state
+		ArrayList<GameState> retVal = new ArrayList<GameState>(); // all states
+		int parentLevel = getLevel(state);
+		
+		// retrieving all possible moves
+		for (int i = 0; i < getMoves(state).size(); i++) {
+			XYLocation loc = (XYLocation) getMoves(state).get(i);
+			
+			// now create the board state that include this location
+			GameState aState = makeMove(state, loc);
+			aState.put("moveMade", loc); // the move taken
+			aState.put("level", new Integer(parentLevel + 1)); // getting deeper
+			retVal.add(aState);
+		}
+		
+		presentState = temp;
+		return retVal;
 	}
 
 	@Override
-	public GameState makeMove(GameState arg0, Object arg1) {
+	public GameState makeMove(GameState state, Object o) {
 		// TODO Auto-generated method stub
 		return null;
 	}
