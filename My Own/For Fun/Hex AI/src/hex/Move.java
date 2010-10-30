abb292470dbeea65d31fb2f4ae368d08400ba09b
erik.sugiarto@gmail.com <erik.sugiarto@gmail.com@052007a7-1b23-c614-a664-fac8de5c04ab>
2010-10-30 17:26:14 +0000

    Hex AI, my own
    partial done for hex board
    
    use hex package from 323
    
    need to test first the board
    
    should DFS be done in normal way or use set path?

---
+++ My Own/For Fun/Hex AI/src/hex/Move.java
@@ -0,0 +1,20 @@
+package hex;
+
+/*
+ *  Referee's implementation of a move - piece and (row, column) position
+ */
+
+public class Move implements Piece {
+	
+	public int Row;
+	public int Col;
+	public int P;   // type of piece in move
+	
+	public Move()
+	{
+		Row = -1;
+		Col = -1;
+		P = EMPTY;
+	}
+	
+}
